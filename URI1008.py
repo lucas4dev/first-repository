a754aa1ad29acf11d3634cb2467d8ca35402f30f
lucas4dev <65988488+lucas4dev@users.noreply.github.com>
2021-10-15 08:13:46 -0300

    Delete URI1008.py

--- URI1008.py
+++
@@ -1,8 +0,0 @@
-numero = int(input())
-horas = int(input())
-saalarioporhora = float(input())
-
-print(f'NUMBER = {numero}')
-salario = horas * saalarioporhora
-
-print(f'SALARY = U$ {salario:.2f}')
